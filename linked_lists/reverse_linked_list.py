f2fcd3ba29f1705cccd0114972f2a3cce7868c84
AugustineKadima <kadimasam@gmail.com>
2022-11-13 18:58:32 +0300

    Add best time to sell stock

--- linked_lists/reverse_linked_list.py
+++ linked_lists/reverse_linked_list.py
@@ -6,11 +6,12 @@ https://leetcode.com/problems/reverse-linked-list/
 def reverse_linked_list(head):
     prev = None
     current = head
-    while head:
+    while current:
         temp =  current.next
         current.next = prev #Reverse
         prev = current #Iterate prev
         current = temp #Iterate current
+
 
     return prev
 
