9b62145357b5f86a36fcd979ed84a47140fc4663
AugustineKadima <kadimasam@gmail.com>
2022-11-17 01:23:15 +0300

    Add graph breath first search

--- graphs/breath_first_search.py
+++ graphs/breath_first_search.py
@@ -1,1 +1,19 @@
 
+graph = {
+    'a':['b','c'],
+    'b':['d'],
+    'c':['e'],
+    'd':['f'],
+    'e':[],
+    'f':[]
+}
+
+def breath_first_print(graph, source):
+    queue = [ source ]
+    while len(queue) > 0:
+        current = queue.pop(0)
+        print(current)
+        for neighbour in graph[current]:
+            queue.append(neighbour)
+
+breath_first_print(graph, 'a')
