5b2c5fbbc6cc77d82cc09cde0ddaeea669076f6b
AugustineKadima <kadimasam@gmail.com>
2022-12-02 12:49:10 +0300

    Add undirected path

--- graphs/undirected_path.py
+++ graphs/undirected_path.py
@@ -22,3 +22,39 @@ graph = {
 }
 
 '''
+edges = [
+    ['i', 'j'],
+    ['k', 'i'],
+    ['m', 'k'],
+    ['k', 'l'],
+    ['o', 'n']
+]
+
+def undirected_path(edges, node_a, node_b):
+    graph = build_graph(edges)
+    return has_path(graph,node_a, node_b, set())
+
+
+def build_graph(edges):
+    graph  = {}
+    for edge in edges:
+        [a, b] = edge
+        if a not in graph: graph[a] = []
+        if b not in graph: graph[b] = []
+        graph[a].append(b)
+        graph[b].append(a)
+    
+    return graph
+
+def has_path(graph, src, dst, visited):
+    if src == dst:
+        return True
+    if src in visited:
+        return False
+    visited.add(src)
+    for neighbour in graph[src]:
+        # if has_path(graph, neighbour, dst, visited) == True: return True
+        if dst == neighbour: return True
+    return False
+
+print(undirected_path(edges, 'i', 'j'))
