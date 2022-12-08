75f3f890013df43feb2ed5f6343e47ec7a6cef0e
AugustineKadima <kadimasam@gmail.com>
2022-12-08 03:27:49 +0300

    Add connected componentss count

--- graphs/connected_components_count.py
+++ graphs/connected_components_count.py
@@ -1,12 +1,29 @@
 '''
 Write a funtion connected_components_count, that takes in an adjacency list of an undirected graph. The function should return the number of connected components within the graph.
 '''
-# def connected_components_count(graph):
-#     visited = set()
-#     count = 0
-#     for node in graph:
-#         if node in visited: return False
-#         visited.add(node)
-#         count += 1
-#         for neigbour in graph[node]:
-            
+def connected_components_count(graph):
+    visited = set()
+    count = 0
+    def dfs(node):
+        for neighbour in graph[node]:
+            if neighbour not in visited:
+                visited.add(neighbour)
+                dfs(neighbour)
+    for node in graph:
+        if node in visited:
+            continue
+        else:
+            visited.add(node)
+            count += 1
+            dfs(node)
+    return count
+
+graph = {
+    'a':['b', 'c'],
+    'b':['a','c'],
+    'c':['a','b'],
+    'd':[],
+    'k':[]
+}
+
+print(connected_components_count(graph))
--- sorting/bubble_sort_two.py
+++ sorting/bubble_sort_two.py
@@ -2,8 +2,6 @@ def sort_arr(arr):
     for i in range(len(arr)):
         for j in range(len(arr) ):
             if arr[j] > arr[i]:
-                temp = arr[j]
-                arr[j] = arr[i]
-                arr[i] = temp
+                arr[i], arr[j] = arr[j], arr[i]
     return arr
 print(sort_arr([5,3,6,3,9,2]))
